18cfc3ed4c4154ad9e2760a949b2f7e51d77d758
balayogi789 <balyogigmcs@gmail.com>
2024-05-20 14:51:09 -0400

    New item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/data/dummy_items.dart';
+import 'package:shopping/widgets/newitem.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+
+  void _addItem(){
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const NewItem()));
+    // remember as here statefulwidget , noneed of adding context in _addItem()
+    // if it statelessWidget as doesnot know about context so instead we write here _addItem()
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
           itemCount: groceryItems.length,
